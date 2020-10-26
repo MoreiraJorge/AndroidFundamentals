d1864fbc3d575c44fecb04c5af5eed39bff11de8
MoreiraJorge <moreirajorge_17@outlook.pt>
2020-10-26 14:59:36 +0000

    Done FP 02

--- FP 02/EX3/Quiz/app/src/main/java/pt/ipp/estg/quiz/graphic.java
+++ FP 02/EX3/Quiz/app/src/main/java/pt/ipp/estg/quiz/graphic.java
@@ -6,25 +6,60 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class graphic extends AppCompatActivity implements View.OnClickListener {
 
-    private TextView correct;
-    private TextView wrong;
+    private int correct;
+    private int wrong;
+    private double total;
+    private BarChart bchart;
+    private TextView mtextView3;
+    private TextView mtextView4;
+
     private Button mbutton3;
+
+    private ImageView mImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_graphic);
 
-        correct = findViewById(R.id.textView3);
-        wrong = findViewById(R.id.textView4);
         mbutton3 = findViewById(R.id.button3);
+        mtextView3 = findViewById(R.id.textView3);
+        mtextView4 = findViewById(R.id.textView4);
+        mImage = findViewById(R.id.imageView2);
 
-        correct.setText(getIntent().getStringExtra("CORRECT"));
-        wrong.setText(getIntent().getStringExtra("WRONG"));
+        bchart = (BarChart) findViewById(R.id.chart2);
+
+        correct = Integer.parseInt(getIntent().getStringExtra("CORRECT"));
+        wrong = Integer.parseInt(getIntent().getStringExtra("WRONG"));
+        total = Integer.parseInt(getIntent().getStringExtra("TOTAL"));
+
+        percentage(correct, wrong, total);
+
+        //set the graphic
+        List<BarEntry> entries = new ArrayList<>();
+        entries.add(new BarEntry(0f, correct));
+        entries.add(new BarEntry(1f, wrong));
+
+        BarDataSet set = new BarDataSet(entries, "BarDataSet");
+
+        BarData data = new BarData(set);
+        data.setBarWidth(0.6f); // set custom bar width
+        bchart.setData(data);
+        bchart.setFitBars(true); // make the x-axis fit exactly all bars
+        bchart.invalidate(); // refresh
 
         mbutton3.setOnClickListener(this);
     }
@@ -63,6 +98,19 @@ public class graphic extends AppCompatActivity implements View.OnClickListener {
     protected void onDestroy(){
         super.onDestroy();
         Log.d("GRAPHIC","onDestroy()");
+    }
+
+    private void percentage(int correct, int wrong, double total){
+        mtextView3.setText("Percentagem correta: " + ((correct / total) * 100) + "%");
+        mtextView4.setText("Percentagem errada: " + ((wrong / total) * 100) + "%");
+
+        if(correct > 8){
+            mImage.setImageResource(R.drawable.a);
+        } else if(correct == 5){
+            mImage.setImageResource(R.drawable.b);
+        } else {
+            mImage.setImageResource(R.drawable.f);
+        }
     }
 
     @Override
