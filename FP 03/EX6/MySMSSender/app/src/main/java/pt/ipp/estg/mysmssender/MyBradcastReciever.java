7e5f8726985934bae30fe43ba92dd56b178ec44f
MoreiraJorge <moreirajorge_17@outlook.pt>
2020-10-28 20:24:15 +0000

    Done FP 03

---
+++ FP 03/EX6/MySMSSender/app/src/main/java/pt/ipp/estg/mysmssender/MyBradcastReciever.java
@@ -0,0 +1,20 @@
+package pt.ipp.estg.mysmssender;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+public class MyBradcastReciever extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        MainActivity activity = (MainActivity) context;
+        if (intent.getAction() == activity.ACTION_SEND_SMS) {
+            Toast.makeText(context, "SMS sent!", Toast.LENGTH_LONG).show();
+        } else if (intent.getAction() == activity.ACTION_SMS_DELIVERED) {
+            Toast.makeText(context, "SMS delivered!", Toast.LENGTH_LONG).show();
+        }
+    }
+
+}
