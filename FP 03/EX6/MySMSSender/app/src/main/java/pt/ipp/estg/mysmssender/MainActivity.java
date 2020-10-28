08f6a6764743389eae82965d0f04279b0592788d
MoreiraJorge <moreirajorge_17@outlook.pt>
2020-10-28 20:56:18 +0000

    Done FP 03

--- FP 03/EX6/MySMSSender/app/src/main/java/pt/ipp/estg/mysmssender/MainActivity.java
+++ FP 03/EX6/MySMSSender/app/src/main/java/pt/ipp/estg/mysmssender/MainActivity.java
@@ -117,6 +117,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                         strings,
                         myPermissionsRequestSendSms);
             }
+
         }
     }
 
