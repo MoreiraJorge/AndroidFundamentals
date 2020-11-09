30b9a6d0ee49460024111ee3c9a18f4164f3a66a
MoreiraJorge <moreirajorge_17@outlook.pt>
2020-11-09 20:11:04 +0000

    Dope p3 and ex2 p1 with tablet bug

---
+++ FP 05/Ex02/Fragments/app/src/main/java/pt/ipp/estg/fragments/fragments/DetailsFragment.java
@@ -0,0 +1,58 @@
+package pt.ipp.estg.fragments.fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import pt.ipp.estg.fragments.R;
+import pt.ipp.estg.fragments.entities.City;
+
+public class DetailsFragment extends Fragment {
+
+    private TextView nameText;
+    private TextView countryText;
+    private TextView descriptionText;
+    private City city;
+
+    public DetailsFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Bundle bundle = getArguments();
+        if(bundle!=null){
+            city= (City) bundle.getSerializable("CITY");
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View v = inflater.inflate(R.layout.fragment_details, container, false);
+
+        nameText = v.findViewById(R.id.textView4);
+        countryText = v.findViewById(R.id.textView5);
+        descriptionText = v.findViewById(R.id.textView);
+
+        nameText.setText(city.getName());
+        countryText.setText(city.getCountry());
+        descriptionText.setText(city.getDescription());
+
+        return v;
+    }
+
+    public void update(City city){
+        nameText.setText(city.getName());
+        countryText.setText(city.getCountry());
+        descriptionText.setText(city.getDescription());
+    }
+
+}
