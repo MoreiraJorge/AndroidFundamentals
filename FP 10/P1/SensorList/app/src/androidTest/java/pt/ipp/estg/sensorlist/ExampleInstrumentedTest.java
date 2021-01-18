e20d407ebd5d15bb47226754cd7d2421944dbd2a
MoreiraJorge <moreirajorge_17@outlook.pt>
2021-01-18 17:23:38 +0000

    Add fp10 and fp11

--- FP 10/P1/SensorList/app/src/androidTest/java/pt/ipp/estg/sensorlist/ExampleInstrumentedTest.java
+++ FP 10/P1/SensorList/app/src/androidTest/java/pt/ipp/estg/sensorlist/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package com.example.sensorlist;
+package pt.ipp.estg.sensorlist;
 
 import android.content.Context;
 
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.example.sensorlist", appContext.getPackageName());
+        assertEquals("pt.ipp.estg.sensorlist", appContext.getPackageName());
     }
 }
--- FP 10/P1/SensorList/app/src/test/java/pt/ipp/estg/sensorlist/ExampleUnitTest.java
+++ FP 10/P1/SensorList/app/src/test/java/pt/ipp/estg/sensorlist/ExampleUnitTest.java
@@ -1,4 +1,4 @@
-package com.example.sensorlist;
+package pt.ipp.estg.sensorlist;
 
 import org.junit.Test;
 
